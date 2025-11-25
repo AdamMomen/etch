8fa45a57c01900337a14c9270bac3a2cf2d252e5
Adam Momen <adammuman81@gmail.com>
2025-11-25 12:51:53 +0800

    Chore clean up

--- client-backup-1763903409/out/preload/index.mjs
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-contextBridge.exposeInMainWorld("electron", {
-  desktopCapturer: {
-    getSources: (options) => ipcRenderer.invoke("desktop-capturer-get-sources", options)
-  }
-});
--- client-backup-1763903409/src/preload/index.ts
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electron', {
-  desktopCapturer: {
-    getSources: (options: Electron.SourcesOptions) =>
-      ipcRenderer.invoke('desktop-capturer-get-sources', options),
-  },
-});
-
